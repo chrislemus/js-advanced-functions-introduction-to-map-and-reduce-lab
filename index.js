1b3b57197c94077e531ddb5d33462719a1960b48
chrislemus <icrislemus@gmail.com>
2021-03-17 09:32:10 -0400

    commit tree 26a63a2461c09979f7c3fda6ba209139706ad4fb

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function mapToNegativize(numbers) {
+  return numbers.map(num => num * -1)
+}
+
+function mapToNoChange(numbers) { return numbers };
+
+function mapToDouble(numbers) {
+  return numbers.map(num => num * 2)
+}
+
+function mapToSquare(numbers) {
+  return numbers.map(num => num ** 2)
+}
+
+function reduceToTotal(numbers, startNum=0) {
+  return numbers.reduce((acc, num) =>  acc + num, startNum)
+}
+function reduceToAllTrue(array) {
+  // return array.reduce((accu, curr) => Boolean(Math.min(accu, curr)))
+  let a;
+  array.forEach((element, idx) => {
+    if(Boolean(element)){
+      if(idx === (array.length - 1))a = true;
+    } else {
+      a = false
+    }
+  });
+  return a
+}
+
+function reduceToAnyTrue(array) {
+  let a;
+  array.forEach((element) => {
+    if(Boolean(element)){
+      a = true;
+    } else {
+      a = false
+    }
+  });
+  return a
+}
